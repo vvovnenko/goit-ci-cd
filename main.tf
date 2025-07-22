296970561881db6a5955680a3aa0c788801adf3c
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2025-07-22 21:28:57 +0300

    change eks config

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "eks" {
   cluster_name    = "eks-cluster-demo"            # Назва кластера
   subnet_ids      = module.vpc.public_subnets     # ID підмереж
   instance_type   = "t3.medium"                    # Тип інстансів
-  desired_size    = 1                             # Бажана кількість нодів
+  desired_size    = 2                             # Бажана кількість нодів
   max_size        = 2                             # Максимальна кількість нодів
   min_size        = 1                             # Мінімальна кількість нодів
 }
